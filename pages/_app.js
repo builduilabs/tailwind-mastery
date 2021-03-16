df75f58704b29670f1a0c0762aa6061e7f7b47bf
Sam Selikoff <sam.selikoff@gmail.com>
2021-03-16 17:13:53 -0400

    WIP

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      ...defaultTheme.colors,
+      gray: {
+        50: "#D6D6D7",
+        100: "#DCDDDE",
+        200: "#8E9298",
+        300: "#72767E",
+        350: "#4F545C",
+        400: "#3B3C43",
+        500: "#35373C",
+        600: "#373940",
+        700: "#303136",
+        800: "#1F2124",
+      },
+      "light-accent": "rgba(255,255,255,.06)",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},

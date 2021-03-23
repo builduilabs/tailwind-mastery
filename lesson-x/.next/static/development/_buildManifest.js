88cc855899c8c6fe026dd8a28b09fb4f9ae7388c
Sam Selikoff <sam.selikoff@gmail.com>
2021-03-23 09:57:41 -0400

    update gitignore

---
+++ lesson-x/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/video-1":["static\u002Fchunks\u002Fpages\u002Fvideo-1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fvideo-1"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

d1fac5a24ba23ce624defe9bc22f61d29f5541b7
Sam Selikoff <sam.selikoff@gmail.com>
2021-03-26 12:55:52 -0400

    Revert index

--- lesson-1/pages/solution.js
+++ lesson-1/pages/solution.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-gray-700 text-white min-h-screen flex items-center justify-center">
-      <div className="max-w-2xl">
+      <div className="max-w-lg">
         <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
           <img
             className="w-10 h-10 rounded-full mr-4"
@@ -9,12 +9,12 @@ export default function Home() {
             alt=""
           />
           <div>
-            <div className="flex items-baseline">
-              <span className="text-green-500 text-sm mr-2 font-medium">
+            <p className="flex items-baseline">
+              <span className="text-green-500 mr-2 text-sm font-medium">
                 adamwathan
               </span>
-              <span className="text-gray-500 text-xs">01/15/2021</span>
-            </div>
+              <span className="text-xs text-gray-500">01/15/2021</span>
+            </p>
             <p className="text-gray-300">
               You should never use something like leading relaxed with a big
               font size, it goes against all typography best practices. Line
@@ -22,14 +22,14 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+        <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
           <p className="text-gray-300 pl-14">
             You can override it in your config if you want but ultimately we
             chose the defaults they did because they let you get results closest
             to what a professional designer would do more easily
           </p>
         </div>
-        <div className="hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+        <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
           <p className="text-gray-300 pl-14">
             Since we changed this in tailwind 2 I’ve almost never used a leading
             class at all

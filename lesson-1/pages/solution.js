3cb815fcade2076f3cecdc985c0616216aab50b5
Sam Selikoff <sam.selikoff@gmail.com>
2021-03-26 10:33:17 -0400

    runthrough v1

--- lesson-1/pages/solution.js
+++ lesson-1/pages/solution.js
@@ -1,43 +1,39 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-700">
+    <div className="bg-gray-700 text-white min-h-screen flex items-center justify-center">
       <div className="max-w-2xl">
-        <div className="flex px-4 py-2 hover:bg-gray-800 hover:bg-opacity-30">
+        <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
           <img
-            className="w-10 h-10 mr-4 rounded-full"
+            className="w-10 h-10 rounded-full mr-4"
             src="/adamwathan.jpeg"
             alt=""
           />
-          <div className="flex flex-col">
-            <div className="flex items-center text-sm font-medium text-green-500">
-              <span className="mr-2">adamwathan</span>
-              <span className="text-xs text-gray-500">01/15/2021</span>
+          <div>
+            <div className="flex items-baseline">
+              <span className="text-green-500 text-sm mr-2 font-medium">
+                adamwathan
+              </span>
+              <span className="text-gray-500 text-xs">01/15/2021</span>
             </div>
-            <div className="text-gray-300">
-              <p>
-                You should never use something like leading-relaxed with a big
-                font size, it goes against all typography best practices. Line
-                height should decrease as font size gets bigger
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="px-4 py-2 hover:bg-gray-800 hover:bg-opacity-30">
-          <div className="text-gray-300 ml-14">
-            <p>
-              You can override it in your config if you want but ultimately we
-              chose the defaults we did because they let you get results closest
-              to what a professional designer would do more easily
+            <p className="text-gray-300">
+              You should never use something like leading relaxed with a big
+              font size, it goes against all typography best practices. Line
+              height should decrease as font size gets bigger
             </p>
           </div>
         </div>
-        <div className="px-4 py-2 hover:bg-gray-800 hover:bg-opacity-30">
-          <div className="text-gray-300 ml-14">
-            <p>
-              Since we changed this in tailwind 2 I’ve almost never used a
-              leading class at all
-            </p>
-          </div>
+        <div className="hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+          <p className="text-gray-300 pl-14">
+            You can override it in your config if you want but ultimately we
+            chose the defaults they did because they let you get results closest
+            to what a professional designer would do more easily
+          </p>
+        </div>
+        <div className="hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+          <p className="text-gray-300 pl-14">
+            Since we changed this in tailwind 2 I’ve almost never used a leading
+            class at all
+          </p>
         </div>
       </div>
     </div>

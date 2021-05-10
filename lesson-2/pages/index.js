0836db8c16976c9383b5ccf038c0ffd4a6025888
Sam Selikoff <sam.selikoff@gmail.com>
2021-05-10 19:21:43 -0400

    End of Lesson 2

--- lesson-2/pages/index.js
+++ lesson-2/pages/index.js
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div>Hello, Tailwind!</div>;
+  return (
+    <div className="flex text-white h-screen">
+      <div className="bg-gray-800 p-4">
+        <div className="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
+          TW
+        </div>
+      </div>
+      <div className="bg-gray-700 w-60 flex flex-col">
+        <div className="p-4 shadow-md">Tailwind CSS</div>
+        <div className="p-4 flex-1">channels</div>
+      </div>
+      <div className="bg-gray-600 flex-1 flex flex-col">
+        <div className="p-4 shadow-md">general</div>
+        <div className="p-4 flex-1">messages</div>
+      </div>
+    </div>
+  );
 }

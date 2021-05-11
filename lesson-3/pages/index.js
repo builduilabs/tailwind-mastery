f05783dff7a719f46bb888ad36293cd373eb0d06
Sam Selikoff <sam.selikoff@gmail.com>
2021-05-11 05:36:39 -0400

    wip

--- lesson-3/pages/index.js
+++ lesson-3/pages/index.js
@@ -2,17 +2,17 @@ export default function Home() {
   return (
     <div className="flex text-white h-screen">
       <div className="bg-gray-800 p-4">
-        <div className="bg-white text-gray-800 h-12 w-12 rounded-full flex items-center justify-center">
+        <div className="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
           TW
         </div>
       </div>
       <div className="bg-gray-700 w-60 flex flex-col">
-        <div className="shadow p-4">Tailwind CSS</div>
-        <div className="p-4 flex-1">Channel list</div>
+        <div className="p-4 shadow-md">Tailwind CSS</div>
+        <div className="p-4 flex-1">channels</div>
       </div>
       <div className="bg-gray-600 flex-1 flex flex-col">
-        <div className="shadow p-4"># general</div>
-        <div className="p-4 flex-">Messages</div>
+        <div className="p-4 shadow-md">general</div>
+        <div className="p-4 flex-1">messages</div>
       </div>
     </div>
   );

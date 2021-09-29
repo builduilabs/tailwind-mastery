fc627b1f4d99576cce9709f7c23a1f1f93dbe325
Sam Selikoff <sam.selikoff@gmail.com>
2021-09-29 14:00:52 -0400

    End of L11

--- lesson-11/tailwind.config.js
+++ lesson-11/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   mode: "jit",
   theme: {
+    boxShadow: {
+      sm: "0 1px 0 rgba(4,4,5,0.2),0 1.5px 0 rgba(6,6,7,0.05),0 2px 0 rgba(4,4,5,0.05)",
+      md: "0 4px 4px rgba(0,0,0,0.16)",
+      lg: "0 8px 16px rgba(0,0,0,0.24)",
+    },
     extend: {
       fontFamily: {
         sans: ["Whitney", "Open Sans", ...defaultTheme.fontFamily.sans],
@@ -19,6 +24,7 @@ module.exports = {
           300: "#8E9297",
           400: "#72767D",
           500: "#5C6067",
+          550: "#4f545c",
           600: "#464950",
           700: "#36393F",
           800: "#2F3136",

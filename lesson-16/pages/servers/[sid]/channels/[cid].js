47a51e52ea292145abf771bbad75de7c4c56b4d1
Sam Selikoff <sam.selikoff@gmail.com>
2021-10-13 11:19:42 -0400

    L16 prep

---
+++ lesson-16/pages/servers/[sid]/channels/[cid].js
@@ -0,0 +1,87 @@
+import * as Icons from "../../../../components/icons";
+import data from "../../../../data.json";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Server() {
+  return (
+    <>
+      <div className="flex flex-col bg-gray-800 w-60">
+        <button className="flex items-center h-12 px-4 font-semibold text-white shadow-sm font-title text-[15px] hover:bg-gray-550/[0.16] transition">
+          <div className="relative w-4 h-4 mr-1">
+            <Icons.Verified className="absolute w-4 h-4 text-gray-550" />
+            <Icons.Check className="absolute w-4 h-4" />
+          </div>
+          Tailwind CSS
+          <Icons.Chevron className="w-[18px] h-[18px] ml-auto opacity-80" />
+        </button>
+
+        <div className="flex-1 overflow-y-scroll font-medium text-gray-300 pt-3 space-y-[21px]">
+          {data["1"].categories.map((category) => (
+            <div key={category.id}>
+              {category.label && (
+                <button className="flex items-center px-0.5 text-xs uppercase font-title tracking-wide hover:text-gray-100 w-full">
+                  <Icons.Arrow className="w-3 h-3 mr-0.5" />
+                  {category.label}
+                </button>
+              )}
+
+              <div className="space-y-0.5 mt-[5px]">
+                {category.channels.map((channel) => (
+                  <ChannelLink channel={channel} key={channel.id} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col flex-1 bg-gray-700">
+        <div className="flex items-center h-12 px-3 shadow-sm">general</div>
+        <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
+          {[...Array(40)].map((_, i) => (
+            <p key={i}>
+              Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Vel saepe laudantium sed reprehenderit incidunt! Hic rem
+              quos reiciendis, fugit quae ratione beatae veniam laborum
+              voluptatem, iusto dolorum, voluptates suscipit quia.
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function ChannelLink({ channel }) {
+  let Icon = channel.icon ? Icons[channel.icon] : Icons.Hashtag;
+  let router = useRouter();
+  let active = +channel.id === +router.query.cid;
+  let state = active
+    ? "active"
+    : channel.unread
+    ? "inactiveUnread"
+    : "inactiveRead";
+  let classes = {
+    active: "text-white bg-gray-550/[0.32]",
+    inactiveUnread:
+      "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]",
+    inactiveRead:
+      "text-white hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]",
+  };
+
+  return (
+    <Link href={`/servers/1/channels/${channel.id}`}>
+      <a
+        className={`${classes[state]} flex items-center px-2 mx-2 py-1 rounded group relative`}
+      >
+        {state === "inactiveUnread" && (
+          <div className="absolute left-0 w-1 h-2 -ml-2 bg-white rounded-r-full"></div>
+        )}
+        <Icon className="w-5 h-5 mr-1.5 text-gray-400" />
+        {channel.label}
+        <Icons.AddPerson className="w-4 h-4 ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+      </a>
+    </Link>
+  );
+}

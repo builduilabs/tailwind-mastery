d204269dddfd1cab4f27bd96d13ab3d76ecd7df3
Sam Selikoff <sam.selikoff@gmail.com>
2021-10-13 12:10:20 -0400

    End of L15

--- lesson-15/pages/servers/[sid]/channels/[cid].js
+++ lesson-15/pages/servers/[sid]/channels/[cid].js
@@ -20,7 +20,7 @@ export default function Server() {
           {data["1"].categories.map((category) => (
             <div key={category.id}>
               {category.label && (
-                <button className="flex items-center px-0.5 text-xs uppercase font-title tracking-wide">
+                <button className="flex items-center px-0.5 text-xs uppercase font-title tracking-wide hover:text-gray-100 w-full">
                   <Icons.Arrow className="w-3 h-3 mr-0.5" />
                   {category.label}
                 </button>
@@ -57,16 +57,27 @@ function ChannelLink({ channel }) {
   let Icon = channel.icon ? Icons[channel.icon] : Icons.Hashtag;
   let router = useRouter();
   let active = +channel.id === +router.query.cid;
+  let state = active
+    ? "active"
+    : channel.unread
+    ? "inactiveUnread"
+    : "inactiveRead";
+  let classes = {
+    active: "text-white bg-gray-550/[0.32]",
+    inactiveUnread:
+      "text-white hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]",
+    inactiveRead:
+      "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]",
+  };
 
   return (
     <Link href={`/servers/1/channels/${channel.id}`}>
       <a
-        className={`${
-          active
-            ? "text-white bg-gray-550/[0.32]"
-            : "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16]"
-        } flex items-center px-2 mx-2 py-1 rounded group`}
+        className={`${classes[state]} flex items-center px-2 mx-2 py-1 rounded group relative`}
       >
+        {state === "inactiveUnread" && (
+          <div className="absolute left-0 w-1 h-2 -ml-2 bg-white rounded-r-full"></div>
+        )}
         <Icon className="w-5 h-5 mr-1.5 text-gray-400" />
         {channel.label}
         <Icons.AddPerson className="w-4 h-4 ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
